d931cd3921ef3f49b499f76f2609148b052e13fb
itmayziii <tommymay37@gmail.com>
2019-12-09 10:16:53 -0500

    feat(initial commit): initial commit with first shot at using this package for extending rules

---
+++ README.md
@@ -0,0 +1,28 @@
+# eslint-plugin-standard-typescript
+
+Standard JS with Typescript playing nice with each other.
+
+Some of the ESLint rules between Typescript and Standard overlap or contradict each other. This package combines the 2 configurations and
+defaults to accepting the Standard JS config over the typescript config when rules overlap.   
+
+## Installation
+
+You'll first need to install [ESLint](http://eslint.org) as well as all the configs and plugins that Standard and Typescript need.
+Unfortunately it is not possible to have a plugin use another plugin as a dependency. It is [recommended by ESLint](https://github.com/eslint/eslint/issues/3458)
+to use peerDependencies instead which is why you are installing so many packages below. 
+
+```
+npm i eslint eslint-plugin-standard-typescript @typescript-eslint/eslint-plugin @typescript-eslint/parser eslint-config-standard eslint-plugin-import eslint-plugin-node eslint-plugin-promise eslint-plugin-standard --save-dev
+```
+
+**Note:** If you installed ESLint globally (using the `-g` flag) then you must also install the other packages above globally.
+
+## Usage
+
+Add `standard-typescript` to the extends section of your ESLint configuration file.
+
+```json
+{
+    "extends": ["plugin:standard-typescript"]
+}
+```
---
+++ dist/index.js
@@ -0,0 +1,38 @@
+module.exports = {
+  environments: {
+    typescript: {
+      parserOptions: {
+        project: 'tsconfig.json',
+        tsconfigRootDir: './'
+      }
+    }
+  },
+  configs: {
+    recommended: {
+      plugins: ['standard-typescript', '@typescript-eslint'],
+      env: ['typescript'],
+      rules: {
+        // Standard turns this rule off.
+        '@typescript-eslint/no-use-before-define': 'off',
+        // Honestly this is sort of preference, but very much aligns with Standard with no semicolons being used.
+        '@typescript-eslint/member-delimiter-style': ['error', {
+          multiline: {
+            delimiter: 'none',
+            requireLast: false
+          },
+          singleline: {
+            delimiter: 'comma',
+            requireLast: false
+          }
+        }],
+        // Standard handles indentation.
+        '@typescript-eslint/indent': 'off',
+        // Standard handles unused vars but it doesn't account for things like Typescript interfaces so we are going to rely on the
+        // Typescript '@typescript-eslint/no-unused-vars' rule here instead.
+        'no-unused-vars': 'off',
+        // Obviously Typescript uses unsupported Node features.
+        'node/no-unsupported-features/es-syntax': 'off'
+      }
+    }
+  }
+}
